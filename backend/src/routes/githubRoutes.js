8c89755e5769392c1f6daded24a69cd733fb81e1
kumarikashish-04 <kashish87897231@gmail.com>
2026-05-22 14:43:20 +0530

    changed

--- backend/src/routes/githubRoutes.js
+++ backend/src/routes/githubRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const { getUserRepositories } = require('../controllers/githubController');
 
-router.get('/repos', auth, getUserRepositories);
+const router = express.Router();
+
+const {
+  getGithubRepos,
+} = require('../controllers/githubController');
+
+router.get('/repos', getGithubRepos);
 
 module.exports = router;
